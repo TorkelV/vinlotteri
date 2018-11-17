0a1edd667a28851ed86767c53916a431e96ceb33
Torkelv <torkel93@gmail.com>
2018-11-17 14:15:28 +0100

    add tickets to previous winners when they win

--- resources/public/js/index.js
+++ resources/public/js/index.js
@@ -26,22 +26,24 @@ var app = new Vue({
             }
         },
         drawTicket() {
-            this.drawing = true;
-            let n = 0;
-            if(this.winner) this.previousWinners.push(this.winner);
-            if (this.winningNumber>=0) this.removeTicket(this.winningNumber);
-            this.wColor = 'red';
-            for(let i =10; i<100; i++){
-                setTimeout(()=>{
-                    n = ~~(Math.random() * (this.tickets.length));
-                    this.winner = this.tickets[n];
-                    this.winningNumber = n;
-                },40000/i)
+            if(this.tickets.length) {
+                this.drawing = true;
+                let n = 0;
+                this.wColor = 'red';
+                for (let i = 10; i < 100; i++) {
+                    setTimeout(() => {
+                        n = ~~(Math.random() * (this.tickets.length));
+                        this.winner = this.tickets[n];
+                        this.winningNumber = n;
+                    }, 40000 / i)
+                }
+                setTimeout(() => {
+                    this.previousWinners.push(this.winner);
+                    this.removeTicket(this.winningNumber);
+                    this.wColor = 'green';
+                    this.drawing = false;
+                }, 4050)
             }
-            setTimeout(()=>{
-                this.wColor = 'green';
-                this.drawing = false;
-            },4000)
         },
         undoDrawing(i) {
             this.tickets = this.tickets.concat(this.previousWinners[i]);
